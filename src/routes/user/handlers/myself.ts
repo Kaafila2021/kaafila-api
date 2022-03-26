8d1a23eea7be4696d16a5fa8a77acecbedb52fad
Marc vergara <marc.vergara.f@gmail.com>
2022-03-26 17:50:34 +0100

    feat(myself): added node token for myself endpoint

--- src/routes/user/handlers/myself.ts
+++ src/routes/user/handlers/myself.ts
@@ -1,23 +1,40 @@
 import handleError from "../../../helpers/handle-error";
 import User from '../../register/schemas/User.schema';
+import axios from 'axios';
 
 const myself = async (req, res) => {
 
-    try {
 
-        const user = await User.findById(req.user.id).select('-password')
+    (async () => {
 
-        res
-            .status(200)
-            .contentType("text/json")
-            .json({
-                error: null,
-                data: { user }
-            });
+        try {
 
-    } catch (err) {
-        handleError(err, res)
-    }
+            const user = await User.findById(req.user.id).select('-password')
+            const body = {
+                "expiresIn": 3600000,
+                "permissions": [
+                    "ipfs.add",
+                    {
+                        "assets.write": "*"
+                    }
+                ],
+                "uid": user._id
+            }
+            const { data } = await axios.post('https://wjcpttgp59.execute-api.us-west-2.amazonaws.com/prd/grant', body, { headers: { 'Authorization': 'djIxzDQCjDTLKAH5A1sLh3TY4PAbqWKMNP8kwETSfyprvy26u' } });
+
+            res
+                .status(200)
+                .contentType("text/json")
+                .json({
+                    error: null,
+                    data: { user, ipfsToken: data.token }
+                });
+
+        } catch (err) {
+            handleError(err, res)
+        }
+
+    })();
 }
 
 export default myself;
